e226999c3f4fe13fa781132d12fab9aca8af896b
Ismail <s104428@ap.be>
2019-10-22 16:57:05 +0200

    added user seed

--- FacturatieKMO/FacturatieKMO.DAL/EF/InvoiceDbInitializer.cs
+++ FacturatieKMO/FacturatieKMO.DAL/EF/InvoiceDbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using FacturatieKMO.BL.Domain;
+using System.Collections.Generic;
 using System.Data.Entity;
 
 namespace FacturatieKMO.DAL.EF
@@ -6,6 +7,12 @@ namespace FacturatieKMO.DAL.EF
     internal class InvoiceDbInitializer
         :DropCreateDatabaseIfModelChanges<InvoiceDbContext>
     {
-       
+        protected override void Seed(InvoiceDbContext context)
+        {
+            List<string> roles = new List<string>();
+            roles.Add("Admin");
+            context.Users.Add(new User(1, "Almaci", "Ismail", "s104428@ap.be", "Sint-Gillis-Waas", roles));
+            context.SaveChanges();
+        }
     }
 }
