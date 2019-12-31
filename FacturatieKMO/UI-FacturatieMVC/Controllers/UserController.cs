8add8f2d85a8edd9fcefcecd643f6e6cbcd6481f
IsmailAlmaci <38719243+IsmailAlmaci@users.noreply.github.com>
2019-12-31 02:54:10 +0100

    views

--- FacturatieKMO/UI-FacturatieMVC/Controllers/UserController.cs
+++ FacturatieKMO/UI-FacturatieMVC/Controllers/UserController.cs
@@ -1,51 +1,84 @@
-﻿using FacturatieKMO.BL;
-using FacturatieKMO.BL.Domain;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
+using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using UI_FacturatieMVC.Models;
 
-namespace AP.UI.Web.MVC.Controllers
+namespace UI_FacturatieMVC.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
-        private ApplicationDbContext ctx = new ApplicationDbContext();
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationDbContext context;
 
-        // GET: Gebruiker
-        [Authorize(Roles = "Admin")]
-        public ActionResult Index()
+        public UserController()
         {
-            IEnumerable<ApplicationUser> users = ctx.Users.AsEnumerable();
-            return View(users);
+            context = new ApplicationDbContext();
         }
 
-        // GET: Gebruiker/Details/5
-        [Authorize(Roles = "Admin")]
+        public UserController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var allUsers = context.Users.ToList();
+            return View(allUsers);
+        }
+
+        // GET: User/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Gebruiker/Create
-        [Authorize(Roles = "Admin")]
+        // GET: User/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Gebruiker/Create
-        [Authorize(Roles = "Admin")]
+        // POST: User/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
             try
             {
                 // TODO: Add insert logic here
-                string name = Request.Form["Name"];
-                string firstName = Request.Form["FirstName"];
-                string email = Request.Form["Email"];
-                string address = Request.Form["Address"];
 
                 return RedirectToAction("Index");
             }
@@ -55,24 +88,19 @@ namespace AP.UI.Web.MVC.Controllers
             }
         }
 
-        // GET: Gebruiker/Edit/5
+        // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            return View(ctx.Users.ElementAt(id));
+            return View();
         }
 
-        // POST: Gebruiker/Edit/5
+        // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
             try
             {
-                string name = Request.Form["Name"];
-                string firstName = Request.Form["FirstName"];
-                string email = Request.Form["Email"];
-                string address = Request.Form["Address"];
-
-                UserDTO user = new UserDTO(id, name, firstName, email, address, null, null);
+                // TODO: Add update logic here
 
                 return RedirectToAction("Index");
             }
@@ -82,21 +110,20 @@ namespace AP.UI.Web.MVC.Controllers
             }
         }
 
-        // GET: Gebruiker/Delete/5
-        [Authorize(Roles = "Admin")]
+        // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Gebruiker/Delete/5
-        [Authorize(Roles = "Admin")]
+        // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
                 // TODO: Add delete logic here
+
                 return RedirectToAction("Index");
             }
             catch
