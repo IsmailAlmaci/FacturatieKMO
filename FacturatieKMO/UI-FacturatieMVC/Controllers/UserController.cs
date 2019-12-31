9109b54152b0b112ab39422c6cbe29e700174422
IsmailAlmaci <38719243+IsmailAlmaci@users.noreply.github.com>
2019-12-31 05:00:01 +0100

    added role

--- FacturatieKMO/UI-FacturatieMVC/Controllers/UserController.cs
+++ FacturatieKMO/UI-FacturatieMVC/Controllers/UserController.cs
@@ -107,7 +107,7 @@ namespace UI_FacturatieMVC.Controllers
 
             if (result.Succeeded)
             {
-                await UserManager.AddToRoleAsync(user.Id, "User");
+                await UserManager.AddToRoleAsync(user.Id, model.UserRoles);
 
                 await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
@@ -116,9 +116,10 @@ namespace UI_FacturatieMVC.Controllers
                 // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                 // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                 // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                return RedirectToAction("Index", "Home");
             }
-                return View();
-            
+
+            return View();
         }
 
         // GET: User/Edit/5
