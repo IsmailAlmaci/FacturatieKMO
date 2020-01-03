4f0ae2676a50c396decb22646ab73cb90774bfc7
IsmailAlmaci <38719243+IsmailAlmaci@users.noreply.github.com>
2020-01-03 01:56:49 +0100

    added reset password

--- FacturatieKMO/UI-FacturatieMVC/Controllers/UserController.cs
+++ FacturatieKMO/UI-FacturatieMVC/Controllers/UserController.cs
@@ -195,5 +195,47 @@ namespace UI_FacturatieMVC.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        //
+        // GET: /Account/ResetPassword
+        [AllowAnonymous]
+        public ActionResult ResetPassword(string code)
+        {
+            return code == null ? View("Error") : View();
+        }
+
+        //
+        // POST: /Account/ResetPassword
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await UserManager.FindByNameAsync(model.Email);
+            if (user == null)
+            {
+                // Don't reveal that the user does not exist
+                return RedirectToAction("ResetPasswordConfirmation", "Account");
+            }
+            var result = await UserManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ResetPasswordConfirmation", "Account");
+            }
+
+            return View();
+        }
+
+        //
+        // GET: /Account/ResetPasswordConfirmation
+        [AllowAnonymous]
+        public ActionResult ResetPasswordConfirmation()
+        {
+            return View();
+        }
     }
 }
